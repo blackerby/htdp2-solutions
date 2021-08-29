024292e6c5f12912a3a3b54549381b5af251c770
blackerby <wmblackerby@gmail.com>
2021-08-29 09:20:09 -0500

    Completed exercise 232

---
+++ intermezzo-2/html-generator-example.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname html-generator-example) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/web-io)
+
+; String String -> ... deeply nested list ...
+; produces a web page with given author and title
+(define (my-first-web-page author title)
+  `(html
+     (head
+       (title ,title)
+       (meta ((http-equiv "content-type")
+              (content "text-html"))))
+     (body
+       (h1 ,title)
+       (p "I, " ,author ", made this page."))))
