f61a335e0bce99b76892f6924581d28878fff827
blackerby <wmblackerby@gmail.com>
2021-09-02 07:13:47 -0500

    Complete exercise 255

---
+++ iii-abstraction/exercise255.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise255) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; [List-of Number] [Number -> Number] -> [List-of Number]
+
+; [List-of String] [String -> String] -> [List-of String]
+
+; [X Y] [List-of X] [X -> Y] -> [List-of Y]
+
+; [List-of IR] [IR -> String] -> [List-of String]
