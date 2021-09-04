b023224fb86f40c14b2a04c637ac1c63425cc3ca
blackerby <wmblackerby@gmail.com>
2021-09-04 10:01:31 -0500

    Complete exercise 264

---
+++ iii-abstraction/exercise264.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise264) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Nelon -> Number
+; determines the largest 
+; number on l
+(define (sup l)
+  (cond
+    [(empty? (rest l))
+     (first l)]
+    [else
+     (local ((define largest-in-rest (sup (rest l))))
+     (if (> (first l)
+            largest-in-rest)
+         (first l)
+         largest-in-rest))]))
+
+(sup (list 2 1 3)) ; evaluate with stepper
