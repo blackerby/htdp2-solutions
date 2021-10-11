9652173deb7037e3780878ad87192fd4f139ccc7
blackerby <wmblackerby@gmail.com>
2021-10-11 16:15:28 -0500

    Complete 431, 432

---
+++ v-generative-recursion/exercise432.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise432) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; skipping because I did not complete exercise 219, but study the solutions below
+
+; https://github.com/S8A/htdp-exercises/blob/master/ex432.rkt
+; https://github.com/atharvashukla/htdp/blob/master/src/432.rkt
+; https://gitlab.com/cs-study/htdp/-/blob/main/05-Generative-Recursion/26-Designing-Algorithms/Exercise-432.rkt
